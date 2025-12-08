6358709196c1365627432356a04f8298ce7ccdc0
Ellison-Lee <ellison2025@163.com>
2025-12-09 01:14:06 +0800

    update

--- CA-hw2/Lawn Mopping.py
+++ CA-hw2/Lawn Mopping.py
@@ -1,9 +1,9 @@
 """
 问题：Lawn Mopping（草坪拖地）
-给定一个长度为n的数组，选择一些元素使得任意两个被选元素之间的距离不超过k，求所选元素的最大和。
+给定一个长度为n的数组，不能连续选择k个元素，求所选元素的最大和。
 
 算法：动态规划 + 单调队列优化
-- 使用DP数组dp[i]表示考虑前i个元素，且第i个位置不选时的最大和
+- 使用DP数组max_sum[i]表示考虑前i个元素，且第i个位置不选时的最大和
 - 使用单调队列维护滑动窗口内的最优决策位置
 - 通过前缀和优化区间和的计算
 
@@ -54,39 +54,39 @@ try:
                 sum_val += val
         print(sum_val)
     
-    # DP数组：dp[i]表示考虑前i个元素，且第i个位置不选时的最大和
-    dp = [0] * (n + 2)
-    dq = deque()
+    # DP数组：max_sum[i]表示考虑前i个元素，且第i个位置不选时的最大和
+    max_sum = [0] * (n + 2)
+    breakpoint = deque()
     
     # 初始化：位置0作为起始断点
-    dq.append(0)
-    dp[0] = 0
+    breakpoint.append(0)
+    max_sum[0] = 0
     
     # 核心迭代过程
     for i in range(1, n + 2):
         # 步骤A - 窗口维护：移除超出窗口范围的位置
-        while dq and dq[0] < i - k - 1:
-            dq.popleft()
+        while breakpoint and breakpoint[0] < i - k - 1:
+            breakpoint.popleft()
         
         # 步骤B - 最优决策
-        if dq:
-            j = dq[0]
+        if breakpoint:
+            j = breakpoint[0]
             # 状态转移：前j个元素的最优解 + j到i-1位置的元素和
-            dp[i] = dp[j] + (prefix[i - 1] - prefix[j])
+            max_sum[i] = max_sum[j] + (prefix[i - 1] - prefix[j])
         
         # 步骤C - 队列更新
         # 从尾部移除所有不如当前位置i优的候选位置
-        while dq:
-            last = dq[-1]
-            # 比较标准：dp[last] - prefix[last] <= dp[i] - prefix[i]
-            if dp[last] - prefix[last] <= dp[i] - prefix[i]:
-                dq.pop()
+        while breakpoint:
+            last = breakpoint[-1]
+            # 比较标准：max_sum[last] - prefix[last] <= max_sum[i] - prefix[i]
+            if max_sum[last] - prefix[last] <= max_sum[i] - prefix[i]:
+                breakpoint.pop()
             else:
                 break
-        dq.append(i)
+        breakpoint.append(i)
     
     # 输出最终结果
-    print(dp[n + 1])
+    print(max_sum[n + 1])
 except EOFError:
     exit(0)
 
